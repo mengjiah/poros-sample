cda9aa868856bb46e0130a425d0c2600fbf06f00
Jiahou Meng <Joe@Jiahous-Mac-Mini.local>
2023-01-04 12:15:51 -0500

    copy files

---
+++ test.py
@@ -0,0 +1,17 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[0])
+print(thislist[-1])
+print(thislist[-3])
+print(thislist[1:4])
+string = "hello world"
+b = "Hello, World!"
+print(b[2:5])
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict["colors"])
+list = [[1, 2, 3], ["a", "b", "c"]]
+print(list[1][1])
